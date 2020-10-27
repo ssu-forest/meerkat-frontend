0c0363fdfa30c99e2ab9b857656020eec520ecdf
Nayeon Kim <whatismyjustice@gmail.com>
2020-10-27 22:20:09 +0900

    update: 카드 기본 디자인 구현
    카드 기본 데이터 매핑 완료
    댓글, 대댓글 매핑 완료
    버튼 매핑

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,16 @@
+import { RedEnvelopeFilled } from '@ant-design/icons';
 import React from 'react';
 
-export default ({ text = '' }) => {
+//import Comment from '../components/Comment';
+
+export default ({
+  boardContents = '',
+  boardWriter = '',
+  dateTime = '',
+  boardTitle = '',
+  like = 0,
+  comments = [],
+}) => {
   return (
     <div
       style={{
@@ -9,7 +19,39 @@ export default ({ text = '' }) => {
         borderRadius: 10,
         backgroundColor: '#fff',
       }}>
-      <span>{text}</span>
+      <p>제목 : {boardTitle}</p>
+      <p>작성자 : {boardWriter}</p>
+      <p>날짜 : {dateTime}</p>
+      <p>게시글내용 : {boardContents}</p>
+      <p>Like : {like}</p>
+      {comments.map((comment, i) => {
+        return (
+          <p
+            key={i}
+            style={{
+              backgroundColor: 'red',
+              margin: 20,
+            }}>
+            <span>
+              - <b>{comment.writer}</b>
+            </span>
+            <span> : {comment.comment}</span>
+            <span> / {comment.datetime}</span>
+            {comment.reply &&
+              comment.reply.map((reComment, i) => {
+                return (
+                  <p key={i}>
+                    <span>
+                      <b> --- {reComment.writer}</b>
+                    </span>
+                    <span> : {reComment.comment}</span>
+                    <span>{reComment.datetime}</span>
+                  </p>
+                );
+              })}
+          </p>
+        );
+      })}
     </div>
   );
 };
--- src/pages/Body.js
+++ src/pages/Body.js
@@ -1,15 +1,71 @@
 import React from 'react';
+import Axios from 'axios';
 
 import Card from '../components/Card';
-
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Input, Button } from 'antd';
+import Colors from '../constants/Colors';
 export default props => {
   const [name, setName] = React.useState(props.value); // hook
+  const [cardList, setCardList] = React.useState([]);
+
+  React.useEffect(() => {
+    Axios.get('https://ian-api.herokuapp.com/test/api').then(reseponse => {
+      const {
+        data: { data },
+      } = reseponse;
+
+      console.log(data);
+
+      setCardList(data);
+    });
+  }, []);
 
   return (
     <div className={'layout'}>
       <div className={'layout-inner'}>
         <div className={'layout-main'}>
-          <Card text={'hello world!'} />
+          {cardList.map((v, i) => {
+            return (
+              <div
+                style={{
+                  margin: 20,
+                }}>
+                <Card
+                  key={i}
+                  boardTitle={v.title}
+                  boardContents={v.contents}
+                  boardWriter={v.writer}
+                  dateTime={v.datetime}
+                  like={v.like}
+                  comments={v.comments}
+                />
+                <Input
+                  style={{
+                    height: 35,
+                    marginBottom: 10,
+                  }}
+                  prefix={<UserOutlined />}
+                  placeholder={'댓글입력'}
+                />
+                <Button
+                  block
+                  style={{
+                    height: 40,
+                    backgroundColor: Colors.mainColor,
+                    marginBottom: 10,
+                    color: '#fff',
+                  }}>
+                  <span
+                    style={{
+                      color: Colors.white,
+                    }}>
+                    입력
+                  </span>
+                </Button>
+              </div>
+            );
+          })}
         </div>
         <div className={'layout-side'}>
           <span>B</span>
